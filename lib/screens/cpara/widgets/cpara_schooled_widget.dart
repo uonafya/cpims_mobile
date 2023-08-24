fe757584e582cd9f0f125c8f64954566e10b84ed
munania <munaniadeno@gmail.com>
2023-08-24 19:32:34 +0300

    school state management V1 done

--- lib/screens/cpara/widgets/cpara_schooled_widget.dart
+++ lib/screens/cpara/widgets/cpara_schooled_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cpims_mobile/screens/cpara/widgets/cpara_safe_widget.dart';
 import 'package:cpims_mobile/screens/cpara/widgets/custom_radio_buttons.dart';
 import 'package:cpims_mobile/screens/registry/organisation_units/widgets/steps_wrapper.dart';
 import 'package:flutter/material.dart';
@@ -25,6 +26,14 @@ class _CparaSchooledWidgetState extends State<CparaSchooledWidget> {
       case "school_going_children":
         setState(() {
           school_going_children = value;
+          if (value == RadioButtonOptions.no) {
+            q9_1_Children_enrooled_in_school = RadioButtonOptions.yes;
+            q9_2_Children_attending_school_regularly = RadioButtonOptions.yes;
+          }
+          if (value == RadioButtonOptions.yes) {
+            q9_1_Children_enrooled_in_school = null;
+            q9_2_Children_attending_school_regularly = null;
+          }
         });
         break;
       case "q9_1_Children_enrooled_in_school":
@@ -40,6 +49,12 @@ class _CparaSchooledWidgetState extends State<CparaSchooledWidget> {
       case "ecde_4_5_children":
         setState(() {
           ecde_4_5_children = value;
+          if (value == RadioButtonOptions.no) {
+            q9_3_Children_attending_ecde = RadioButtonOptions.yes;
+          }
+          if (value == RadioButtonOptions.yes) {
+            q9_3_Children_attending_ecde = null;
+          }
         });
         break;
       case "q9_3_Children_attending_ecde":
@@ -76,27 +91,38 @@ class _CparaSchooledWidgetState extends State<CparaSchooledWidget> {
                 'Benchmark 9: All school-aged children (4-17) and adolescents aged 18-20 enrolled in school in the household regularly attended school and progressed during the last year.',
           ),
           const SizedBox(height: small_height),
+
+///////////////////////////////////////////
+// First Main Question
           MainCardQuestion(
               card_question:
                   "Are there school going children in this Household ?",
-              selectedOption: (value) { 
+              selectedOption: (value) {
                 // Update the state of the question
                 updateQuestion("school_going_children", value);
               }),
 
 // Question 9.1 - 9.2
 // Question 9.1
-          OtherQuestions(
+          if (school_going_children == RadioButtonOptions.no)
+            const SkipQuestion()
+          else
+            OtherQuestions(
               other_question:
                   "9.1 Are all school aged children (6-17) enrolled in school? (And out of school OVC aged 15-20 years engaged in approved economic intervention?*",
               selectedOption: (value) {
                 // Update the state of the question
                 updateQuestion("q9_1_Children_enrooled_in_school", value);
               },
-              NaAvailable: false),
+              NaAvailable: false,
+              groupValue: q9_1_Children_enrooled_in_school,
+            ),
 
 // Question 9.2
-          OtherQuestions(
+          if (school_going_children == RadioButtonOptions.no)
+            const SkipQuestion()
+          else
+            OtherQuestions(
               other_question:
                   "9.2 Are the enrolled children attending school regularly? (i.e. have not missed school for more than five school days in a month). Probe the trend of absence). Verify with the school attendance tracking tool where applicable)*",
               selectedOption: (value) {
@@ -104,7 +130,9 @@ class _CparaSchooledWidgetState extends State<CparaSchooledWidget> {
                 updateQuestion(
                     "q9_2_Children_attending_school_regularly", value);
               },
-              NaAvailable: false),
+              NaAvailable: false,
+              groupValue: q9_2_Children_attending_school_regularly,
+            ),
 
 // General Statement
           const Text(
@@ -133,37 +161,44 @@ class _CparaSchooledWidgetState extends State<CparaSchooledWidget> {
 
 // Question 9.3 - 9.4
 // Question 9.3
-          OtherQuestions(
+          if (ecde_4_5_children == RadioButtonOptions.no)
+            const SkipQuestion()
+          else
+            OtherQuestions(
               other_question:
                   "9.3 Is your child (4–5-year-old) attending ECDE?* ",
               selectedOption: (value) {
                 // Update the state of the question
                 updateQuestion("q9_3_Children_attending_ecde", value);
               },
-              NaAvailable: false),
+              NaAvailable: false,
+              groupValue: q9_3_Children_attending_ecde,
+            ),
 
 // Question 9.4
           OtherQuestions(
-              other_question:
-                  "9.4 Have all the enrolled children progressed/graduated from one level to the other in the last school calendar year? Note: if possible, please ask to see report card.*  ",
-              selectedOption: (value) {
-                // Update the state of the question
-                updateQuestion(
-                    "q9_4_Children_progressed_from_one_level_to_another",
-                    value);
-                print("This value2 is $value");
-
-              },
-              NaAvailable: true,
-              divider: true),
+            other_question:
+                "9.4 Have all the enrolled children progressed/graduated from one level to the other in the last school calendar year? Note: if possible, please ask to see report card.*  ",
+            selectedOption: (value) {
+              // Update the state of the question
+              updateQuestion(
+                  "q9_4_Children_progressed_from_one_level_to_another", value);
+            },
+            NaAvailable: true,
+            divider: true,
+            groupValue: q9_4_Children_progressed_from_one_level_to_another,
+          ),
 
 // Benchmark score
           BenchMarkQuestion(
+              groupValue: allShouldBeYes([
+                q9_1_Children_enrooled_in_school,
+                q9_2_Children_attending_school_regularly,
+                q9_3_Children_attending_ecde,
+                q9_4_Children_progressed_from_one_level_to_another
+              ], "Last Benchmark school"),
               benchmark_question: "Has the household achieved this benchmarks?",
-              selectedOption: (value) {
-                // Update the state of the question
-                updateQuestion("benchmark_score", value);
-              }),
+              selectedOption: (value) {}),
 
           const Column(
             children: [
@@ -211,7 +246,8 @@ class GoalWidget extends StatelessWidget {
         children: [
           Text(
             title,
-            style: const TextStyle(fontWeight: goal_weight, fontSize: goal_font),
+            style:
+                const TextStyle(fontWeight: goal_weight, fontSize: goal_font),
           ),
           const SizedBox(
             height: 10,
@@ -273,98 +309,122 @@ class MainCardQuestion extends StatelessWidget {
   }
 }
 
-const question_font_Size = 18.0;
-const question_font_weight = FontWeight.w500;
-const small_height = 10.0;
-const large_height = 25.0;
-
-class OtherQuestions extends StatelessWidget {
-  final bool NaAvailable;
-  final String other_question;
-  final bool divider;
-  final Function(RadioButtonOptions?) selectedOption;
-
-  const OtherQuestions(
-      {super.key,
-      required this.other_question,
-      required this.selectedOption,
-      this.divider = false,
-      this.NaAvailable = false});
+class SkipQuestion extends StatelessWidget {
+  const SkipQuestion({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
-        if (divider == true) const Divider(),
-        const SizedBox(
-          height: large_height,
-        ),
-        Text(
-          other_question,
-          style: const TextStyle(
-            fontSize: question_font_Size,
-            fontWeight: question_font_weight,
-          ),
-        ),
-        const SizedBox(
-          height: small_height,
-        ),
-        CustomRadioButton(
-          isNaAvailable: NaAvailable,
-          optionSelected: (value) => selectedOption(value),
-        ),
-        const SizedBox(
-          height: large_height,
-        ),
+        Text("Skipped Question"),
       ],
     );
   }
 }
 
-class BenchMarkQuestion extends StatelessWidget {
-  final String benchmark_question;
-  final Function(RadioButtonOptions?) selectedOption;
-  const BenchMarkQuestion(
-      {super.key,
-      required this.benchmark_question,
-      required this.selectedOption});
+// class OtherQuestions extends StatelessWidget {
+//   final bool NaAvailable;
+//   final String other_question;
+//   final bool divider;
+//   final Function(RadioButtonOptions?) selectedOption;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: grey,
-            borderRadius: BorderRadius.circular(5),
-            // border:  const Border(
-            //   left: BorderSide(
-            //   color: greyBorder,
-            //   width: 2,
-            //   ),
-            // ),
-          ),
-          child: Column(
-            children: [
-              Text(
-                benchmark_question,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomRadioButton(
-                isNaAvailable: false,
-                optionSelected: (value) => selectedOption(value),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
+//   const OtherQuestions(
+//       {super.key,
+//       required this.other_question,
+//       required this.selectedOption,
+//       this.divider = false,
+//       this.NaAvailable = false});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         if (divider == true) const Divider(),
+//         const SizedBox(
+//           height: large_height,
+//         ),
+//         Text(
+//           other_question,
+//           style: const TextStyle(
+//             fontSize: question_font_Size,
+//             fontWeight: question_font_weight,
+//           ),
+//         ),
+//         const SizedBox(
+//           height: small_height,
+//         ),
+//         CustomRadioButton(
+//           isNaAvailable: NaAvailable,
+//           optionSelected: (value) => selectedOption(value),
+//         ),
+//         const SizedBox(
+//           height: large_height,
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// A function that computes whether the final result of a section is yes or no given the values of the members
+RadioButtonOptions allShouldBeYes(
+    List<RadioButtonOptions?> members, String message) {
+  debugPrint(members.toString() + message);
+  // If all the values are yes return RadioButtonOptions.yes, if not return RadioButtonOptions.no
+  if (members.isEmpty) {
+    return RadioButtonOptions.no;
+  } else if (members
+      .any((element) => element == RadioButtonOptions.no || element == null)) {
+    return RadioButtonOptions.no;
+  } else {
+    return RadioButtonOptions.yes;
   }
 }
+
+
+// class BenchMarkQuestion extends StatelessWidget {
+//   final String benchmark_question;
+//   final Function(RadioButtonOptions?) selectedOption;
+//   const BenchMarkQuestion(
+//       {super.key,
+//       required this.benchmark_question,
+//       required this.selectedOption});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Container(
+//           padding: const EdgeInsets.all(8),
+//           decoration: BoxDecoration(
+//             color: grey,
+//             borderRadius: BorderRadius.circular(5),
+//             // border:  const Border(
+//             //   left: BorderSide(
+//             //   color: greyBorder,
+//             //   width: 2,
+//             //   ),
+//             // ),
+//           ),
+//           child: Column(
+//             children: [
+//               Text(
+//                 benchmark_question,
+//                 style: const TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.w300,
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 10,
+//               ),
+//               CustomRadioButton(
+//                 isNaAvailable: false,
+//                 optionSelected: (value) => selectedOption(value),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
