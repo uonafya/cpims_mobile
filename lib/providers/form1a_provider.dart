0340be4e5376577a4e781b2314e7273644664598
blackcode-creator <legacyitsolution@gmail.com>
2023-08-25 20:11:09 +0300

    added the provider as state management

--- lib/providers/form1a_provider.dart
+++ lib/providers/form1a_provider.dart
@@ -1,16 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:multi_dropdown/models/value_item.dart';
 
-class CriticalFormDate {
+class CriticalFormData {
   late List<ValueItem> selectedEvents;
   late DateTime selectedDate;
-  late String domainId;
 
-  CriticalFormDate({required this.selectedEvents, required this.selectedDate});
+  CriticalFormData({required this.selectedEvents, required this.selectedDate});
+}
+
+class ServiceFormData {
+  late List<ValueItem> selectedDomain;
+  late List<ValueItem> selectedService;
+  late DateTime selectedEventDate;
+
+  ServiceFormData(
+      {required this.selectedDomain,
+      required this.selectedEventDate,
+      required this.selectedService});
 }
 
 class Form1AProvider extends ChangeNotifier {
+  // <<<<<<<<<<<critical events >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  final CriticalFormData _criticalFormData =
+      CriticalFormData(selectedEvents: [], selectedDate: DateTime.now());
+
+  // <<<<<<<<<<<< Service >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  final ServiceFormData _serviceFormData = ServiceFormData(
+      selectedDomain: [],
+      selectedEventDate: DateTime.now(),
+      selectedService: []);
+//<<<<<<<<<<<<<<<<<<<<<Critical events >>>>>>>>>>>>>>>>>>>>>
+
+  CriticalFormData get criticalFormData => _criticalFormData;
+  ServiceFormData get serviceFormData => _serviceFormData;
+
+  // <<<<<<<<<<<<<<<<<<<Services >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // <<<<<<<<<<<<<<<Set Methods >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // <<<<<<<<<<<<<<<Critical methods >>>>>>>>>>>>>>>>>>>>>>>>
   void setSelectedEvents(List<ValueItem> selectedEvents) {
+    _criticalFormData.selectedEvents.clear();
+    _criticalFormData.selectedEvents.addAll(selectedEvents);
     notifyListeners();
+  }
+
+  void setEventSelectedDate(DateTime selectedDate) {
+    criticalFormData.selectedDate = selectedDate;
+    notifyListeners();
+  }
+
+  // <<<<<<<<<<<<<<<<<<<<Services >>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // <<<<<<<<<<<<<<<Domain >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  void setSelectedDomain(List<ValueItem> selectedDomain) {
+    _serviceFormData.selectedDomain.clear();
+    _serviceFormData.selectedDomain.addAll(selectedDomain);
+    notifyListeners();
+  }
+
+  void setSelectedSubDomain(List<ValueItem> selectedService) {
+    _serviceFormData.selectedService.clear();
+    _serviceFormData.selectedService.addAll(selectedService);
+    notifyListeners();
+  }
+
+  void setServiceSelectedDate(DateTime selectedEventDate) {
+    _serviceFormData.selectedEventDate = selectedEventDate;
+  }
+
+  // <<<<<<<<<<<<<<<Submit critical >>>>>>>>>>>>>>>>>>>
+  void submitCriticalData() {
+    String formattedDate =
+        DateFormat('yyyy-MM-dd').format(_criticalFormData.selectedDate);
+
+    List<Map<String, dynamic>> criticalEvents = [];
+    Map<String, dynamic> criticalEvent = {};
+    for (var valueItem in _criticalFormData.selectedEvents) {
+      criticalEvent["event_date"] = formattedDate;
+      criticalEvent["event_id"] = valueItem.value;
+      criticalEvents.add(criticalEvent);
+    }
+    print(criticalEvents);
+  }
+
+  void submitServicesData() {
+    List<Map<String, String>> data = [];
+    for (var valueItem in _serviceFormData.selectedDomain) {
+      String domainId = valueItem.value ?? '';
+      for (var serviceItem in _serviceFormData.selectedService) {
+        String serviceId = serviceItem.value ?? '';
+        Map<String, String> item = {
+          'domain_id': domainId,
+          'service_id': serviceId,
+        };
+        data.add(item);
+      }
+    }
+
+    print(data);
+  }
+
+  void submitCriticalServices() {
+    List<Map<String, String>> data = [];
+    for (var valueItem in _serviceFormData.selectedDomain) {
+      String domainId = valueItem.value ?? '';
+      for (var serviceItem in _serviceFormData.selectedService) {
+        String serviceId = serviceItem.value ?? '';
+        Map<String, String> item = {
+          'domain_id': domainId,
+          'service_id': serviceId,
+        };
+        data.add(item);
+      }
+    }
   }
 }
--- lib/screens/forms/form1a/utils/form_1a_options.dart
+++ lib/screens/forms/form1a/utils/form_1a_options.dart
@@ -1,30 +1,110 @@
+// <<<<<<<<<<< Critical Events >>>>>>>>>>>>>>>>>>>
 List<Map> optionsEvents = [
   {
     'event_id': 'OCE1',
-    'description': 'Child Pregnant',
+    'event_description': 'Child Pregnant',
   },
   {
     'event_id': 'OCE2',
-    'description': 'Child not Adhering to ARVS',
+    'event_description': 'Child not Adhering to ARVS',
   },
   {
     'event_id': 'OCE3',
-    'description': 'Child Malnourished',
+    'event_description': 'Child Malnourished',
   },
   {
     'event_id': 'OCE4',
-    'description': 'Child HIV status Changed',
+    'event_description': 'Child HIV status Changed',
   },
   {
     'event_id': 'OCE5',
-    'description': 'Child acquired opportunistic infection',
+    'event_description': 'Child acquired opportunistic infection',
   },
   {
     'event_id': 'OCE6',
-    'description': 'Child missed HIV clinic appointment',
+    'event_description': 'Child missed HIV clinic appointment',
   },
   {
     'event_id': 'OCE7',
-    'description': 'Child headed household',
+    'event_description': 'Child headed household',
+  },
+];
+
+// <<<<<<<<<<<<< Services >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+List<Map> optionDomains = [
+  {
+    'domain_id': 'OC1',
+    'domain_description': 'Education - (Schooled)',
+  },
+  {
+    'domain_id': 'OC2',
+    'domain_description': 'Health and Nutrition - (Healthy)',
+  },
+  {
+    'domain_id': 'OC3',
+    'domain_description': 'Economic Strengthening - (Stable)',
+  }
+];
+List<Map> optionSubDomains = [
+  {
+    'service_id': 'S1',
+    'domain_id': 'OC1',
+    'service_description': 'Enrolled in ECD'
+  },
+  {
+    'service_id': 'S2',
+    'domain_id': 'OC1',
+    'service_description':
+        'Re-enrolled back to school (including teenage mothers)'
+  },
+  {
+    'service_id': 'S3',
+    'domain_id': 'OC1',
+    'service_description':
+        'Adolescent(15 - 20 yrs) enrolled in an approved HES intervention including VTC'
+  },
+  {'service_id': 'S4', 'service_description': 'School fees paid'},
+  {
+    'service_id': 'S5',
+    'domain_id': 'OC1',
+    'service_description':
+        'Complete referral for bursary or other educational support'
+  },
+  {
+    'service_id': 'S7',
+    'domain_id': 'OC1',
+    'service_description': 'School levies'
+  },
+  {
+    'service_id': 'Sa1',
+    'domain_id': 'OC2',
+    'service_description': 'Facilitated to obtain HIV test'
+  },
+  {
+    'service_id': 'Sa2',
+    'domain_id': 'OC2',
+    'service_description': 'Complete a referral for HIV test'
+  },
+  {
+    'service_id': 'Sa3',
+    'domain_id': 'OC2',
+    'service_description': 'Complete a referral for Early Infact Diagnosis(EID)'
+  },
+  {
+    'service_id': 'Sb1',
+    'domain_id': 'OC3',
+    'service_description': 'OVC Provided with business support(Startup Kit)'
+  },
+  {
+    'service_id': 'Sb2',
+    'domain_id': 'OC3',
+    'service_description':
+        'OVC completed referral for business support(Startup Kit)'
+  },
+  {
+    'service_id': 'Sb3',
+    'domain_id': 'OC3',
+    'service_description': 'OVC Linked to saving group'
   },
 ];
