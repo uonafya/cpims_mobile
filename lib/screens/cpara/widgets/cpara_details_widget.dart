08d949a17f7a6664738349ef0ffe130d78e240f2
Ted Mwai <teddymwai11@gmail.com>
2023-11-05 18:38:34 +0300

    Fix: lastDate value

--- lib/screens/cpara/widgets/cpara_details_widget.dart
+++ lib/screens/cpara/widgets/cpara_details_widget.dart
@@ -29,9 +29,7 @@ class _CparaDetailsWidgetState extends State<CparaDetailsWidget> {
   RadioButtonOptions? hasHivExposedInfant;
   RadioButtonOptions? hasPregnantOrBreastfeedingWoman;
 
-
-  List<CaseLoadModel> children = [
-  ];
+  List<CaseLoadModel> children = [];
 
   @override
   void initState() {
@@ -49,8 +47,7 @@ class _CparaDetailsWidgetState extends State<CparaDetailsWidget> {
             detailModel.hasHivExposedInfant!);
     // dateOfAssessment = detailModel.dateOfAssessment == null ? dateOfAssessment : DateTime.parse(detailModel.dateOfAssessment!);
     // dateOfLastAssessment = detailModel.dateOfLastAssessment == null ? dateOfLastAssessment : DateTime.parse(detailModel.dateOfLastAssessment!);
-    List<CaseLoadModel> models =
-        context.read<CparaProvider>().children ?? [];
+    List<CaseLoadModel> models = context.read<CparaProvider>().children ?? [];
     children = models;
 
     super.initState();
@@ -95,7 +92,8 @@ class _CparaDetailsWidgetState extends State<CparaDetailsWidget> {
             print('Date selected: $date');
             DetailModel detailModel =
                 context.read<CparaProvider>().detailModel ?? DetailModel();
-            context.read<CparaProvider>().updateDetailModel(detailModel.copyWith(dateOfAssessment: date.toString()));
+            context.read<CparaProvider>().updateDetailModel(
+                detailModel.copyWith(dateOfAssessment: date.toString()));
           },
         ),
         const Divider(
@@ -144,9 +142,12 @@ class _CparaDetailsWidgetState extends State<CparaDetailsWidget> {
           physics: const NeverScrollableScrollPhysics(),
           itemCount: children.length,
           itemBuilder: (context, index) {
-            return ExpansionTile(title: Text('${children[index].ovcFirstName!} ${children[index].ovcSurname!}'), children: [
-              ChildCard(childDetails: children[index]),
-            ]);
+            return ExpansionTile(
+                title: Text(
+                    '${children[index].ovcFirstName!} ${children[index].ovcSurname!}'),
+                children: [
+                  ChildCard(childDetails: children[index]),
+                ]);
           },
         ),
         QuestionWidget(
@@ -241,7 +242,7 @@ class _DateTextFieldState extends State<DateTextField> {
             context: context,
             initialDate: DateTime.now(),
             firstDate: DateTime(2000),
-            lastDate: DateTime(2101),
+            lastDate: DateTime.now(),
           ).then((pickedDate) {
             if (pickedDate != null && mounted) {
               setState(() {
@@ -280,14 +281,12 @@ class _TextViewsColumnState extends State<TextViewsColumn> {
 
     caseLoadModel =
         context.read<CparaProvider>().caseLoadModel ?? CaseLoadModel();
-
   }
 
   @override
   Widget build(BuildContext context) {
     final SummaryDataModel dashData =
-    context.select((UIProvider provider) => provider.getDashData);
-
+        context.select((UIProvider provider) => provider.getDashData);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -380,7 +379,8 @@ class ChildCard extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('${childDetails.ovcFirstName!} ${childDetails.ovcSurname!}'),
+                Text(
+                    '${childDetails.ovcFirstName!} ${childDetails.ovcSurname!}'),
                 Text("${calculateAge(childDetails.dateOfBirth!)} years"),
               ],
             ),
