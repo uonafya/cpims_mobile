9d1d2e4e845eb2c8376cbef553d3737d6f7bef86
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-08 16:43:53 +0300

    feat: Adding cpims id to Unapproved cpara model

--- lib/providers/cpara/unapproved_cpara_database.dart
+++ lib/providers/cpara/unapproved_cpara_database.dart
@@ -1,7 +1,12 @@
+import 'package:cpims_mobile/providers/db_provider.dart';
 import 'package:cpims_mobile/screens/cpara/model/cpara_model.dart';
+import 'package:sqflite/sqflite.dart';
+
+var cannot_store_unapproved_cpara = "Could Not Store Unapproved CPARA Data";
 
 class UnapprovedCparaModel extends CparaModel {
   final String message;
+  final String cpmis_id;
 
   UnapprovedCparaModel({
     required super.appFormMetaData,
@@ -12,6 +17,7 @@ class UnapprovedCparaModel extends CparaModel {
     required super.schooled,
     required super.stable,
     required super.uuid,
-    required this.message
+    required this.message,
+    required this.cpmis_id,
   });
 }
