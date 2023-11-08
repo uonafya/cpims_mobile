95ef8261ecba6036fa0e1fdc442c9b6a1a48e4b4
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-08 13:20:44 +0300

    fix: Extending right class

--- lib/providers/cpara/storing_unaproved_cpara.dart
+++ lib/providers/cpara/storing_unaproved_cpara.dart
@@ -1,8 +1,10 @@
-// import 'package:cpims_mobile/Models/unapproved_cpara_model.dart';
-// import 'package:sqflite/sqflite.dart';
+import 'package:cpims_mobile/Models/unapproved_caseplan_form_model.dart';
+import 'package:cpims_mobile/Models/unapproved_cpara_model.dart';
+import 'package:cpims_mobile/providers/cpara/unapproved_cpara_database.dart';
+import 'package:sqflite/sqflite.dart';
 
-// Future<void> storeUnapprovedCPARAToDb(Database db, UnapprovedCparaModel model) async {
+Future<void> storeUnapprovedCPARAToDb(Database db, UnapprovedCPARADatabase model) async {
+  // Store questions from each model
+}
 
-// }
-
-// Future<void> storeUnapprovedCparaQuestions(Database db, )
+Future<void> storeUnapprovedCparaQuestions(Database db, )
--- lib/providers/cpara/unapproved_cpara_database.dart
+++ lib/providers/cpara/unapproved_cpara_database.dart
@@ -1,18 +1,17 @@
-// Due to the large difference between how the CPARA model stores CPARA data
-// and how the database stores CPARA data it is easier to create an intermediary
-// class to bridge the gap
-import 'package:cpims_mobile/screens/cpara/model/db_model.dart';
+import 'package:cpims_mobile/screens/cpara/model/cpara_model.dart';
 
-class UnapprovedCPARADatabase extends CPARADatabase {
+class UnapprovedCparaModel extends CparaModel {
   final String message;
 
-  UnapprovedCPARADatabase(
-      {required super.appFormMetaData,
-      required super.childQuestions,
-      required super.cparaFormId,
-      required super.dateOfEvent,
-      required super.ovcCpimsId,
-      required super.listOfSubOvcs,
-      required super.questions,
-      required this.message});
+  UnapprovedCparaModel({
+    required super.appFormMetaData,
+    required super.detail,
+    required super.health,
+    required super.ovcSubPopulations,
+    required super.safe,
+    required super.schooled,
+    required super.stable,
+    required super.uuid,
+    required this.message
+  });
 }
