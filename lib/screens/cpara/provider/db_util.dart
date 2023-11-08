9c74a75f51a6e1cb0df441c2e478d8f091f7fc60
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-08 12:33:43 +0300

    move code to initialize unapproved tables

--- lib/screens/cpara/provider/db_util.dart
+++ lib/screens/cpara/provider/db_util.dart
@@ -16,6 +16,29 @@ import '../model/cpara_model.dart';
 import '../model/sub_ovc_child.dart';
 import '../widgets/cpara_stable_widget.dart';
 
+// Code for creating unapproved CPARA tables
+Future<void> createUnapprovedCparaTables(Database db) async {
+  // Creating table to store unapproved CPARA form
+  await db.execute('''
+    CREATE TABLE IF NOT EXISTS UnapprovedCPARA(
+      id INTEGER PRIMARY KEY,
+      date_of_event TEXT
+    )
+    ''');
+
+  // Creating table to store answers of the form
+  await db.execute('''
+    CREATE TABLE IF NOT EXISTS UnapprovedCPARAAnswers(
+      id INTEGER PRIMARY KEY,
+      question_code TEXT,
+      answer_id TEXT,
+      ovc_cpims_id TEXT,
+      form_id INTEGER,
+      FOREIGN KEY (form_id) REFERENCES UnapprovedCPARA(id)
+    )
+    ''');
+}
+
 Future<List<CPARADatabase>> getUnsyncedForms(Database db) async {
   try {
     List<CPARADatabase> forms = [];
