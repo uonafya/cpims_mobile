1d93fc0cce20736c551b6f9c250b8b2d4220faae
samrasugu <mokuasamr@gmail.com>
2023-11-09 13:29:45 +0300

    fix: cparaDatabase errors

--- lib/screens/cpara/model/db_model.dart
+++ lib/screens/cpara/model/db_model.dart
@@ -37,7 +37,9 @@ class CPARADatabaseQuestions {
 
   factory CPARADatabaseQuestions.fromJSON(Map<String, dynamic> json) {
     return CPARADatabaseQuestions(
-        questionCode: json['questionid'], answerId: json['answer']);
+      questionCode: json['questionid'],
+      answerId: json['answer'],
+    );
   }
 
   Map<String, dynamic> toJSON() {
@@ -54,14 +56,13 @@ class CPARADatabase {
   AppFormMetaData appFormMetaData;
   List<SubOvcChild> listOfSubOvcs;
 
-  CPARADatabase(
-      {
-        this.cpara_form_id = 0,
-        this.ovc_cpims_id = "",
-        this.date_of_event = "",
-        this.questions = const [],
-        this.childQuestions = const [],
-      this.appFormMetaData = const AppFormMetaData(),
-        this.listOfSubOvcs = const []
-      });
+  CPARADatabase({
+    this.cparaFormId = 0,
+    this.ovcCpimsId = "",
+    this.dateOfEvent = "",
+    this.questions = const [],
+    this.childQuestions = const [],
+    this.appFormMetaData = const AppFormMetaData(),
+    this.listOfSubOvcs = const [],
+  });
 }
