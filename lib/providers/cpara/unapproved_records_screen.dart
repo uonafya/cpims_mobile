14dd8d4a741448315c033442416b2e0a2970c24a
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-10 15:27:52 +0300

    feat: Provider that will control the list of unapproved CPARAs

---
+++ lib/providers/cpara/unapproved_records_screen.dart
@@ -0,0 +1,23 @@
+import 'package:cpims_mobile/providers/cpara/unapproved_cpara_database.dart';
+import 'package:flutter/cupertino.dart';
+
+class UnapprovedRecordsScreenProvider extends ChangeNotifier {
+  // Store the list of the unapproved cparas that we are monitoring
+  List<UnapprovedCparaModel>? _unapprovedCparas;
+
+  // Sets the list of CPARAs
+  set unapprovedCparas(List<UnapprovedCparaModel> cparaModels) {
+    _unapprovedCparas = cparaModels;
+    notifyListeners();
+  }
+
+  // Remove an item from list of CPARAs
+  void removeUnapprovedCpara(UnapprovedCparaModel cparaModel) {
+    if (_unapprovedCparas == null) {
+      return;
+    } else {
+      _unapprovedCparas?.remove(cparaModel);
+      notifyListeners();
+    }
+  }
+}
