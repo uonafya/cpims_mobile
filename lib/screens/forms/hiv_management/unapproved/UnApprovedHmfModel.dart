6c8fbf30ad07c67dd81b5f9d170aec8fa24989c5
agesa3 <collinsagesa2000@gmail.com>
2024-04-08 08:50:43 +0300

    able to cache the new hmf to db

--- lib/screens/forms/hiv_management/unapproved/UnApprovedHmfModel.dart
+++ lib/screens/forms/hiv_management/unapproved/UnApprovedHmfModel.dart
@@ -1,228 +1,155 @@
 import '../../../../utils/app_form_metadata.dart';
 import '../models/hiv_management_form_model.dart';
 
-class UnApprovedHivManagementForm {
-  String? adherenceId;
-  int? ovcCpimsId;
-  String? hivMgmt1A;
-  String? hivMgmt1B;
-  String? hivMgmt1C;
-  String? hivMgmt1D;
-  bool? hivMgmt1E;
-  String? hivMgmt1EDate;
-  bool? hivMgmt1F;
-  String? hivMgmt1FDate;
-  bool? hivMgmt1G;
-  String? hivMgmt1GDate;
-  String? hivMgmt2A;
-  String? hivMgmt2B;
-  String? hivMgmt2C;
-  dynamic hivMgmt2D;
-  String? hivMgmt2E;
-  String? hivMgmt2F;
-  String? hivMgmt2G;
-  String? hivMgmt2H2;
-  dynamic hivMgmt2H1;
-  String? hivMgmt2H3;
-  String? hivMgmt2H4;
-  String? hivMgmt2H5;
-  String? hivMgmt2I1;
-  String? hivMgmt2IDate;
-  String? hivMgmt2J;
-  String? hivMgmt2K;
-  String? hivMgmt2L1;
-  String? hivMgmt2L2;
-  List<String?>? hivMgmt2M;
-  String? hivMgmt2N;
-  bool? hivMgmt2O1;
-  String? hivMgmt2O2;
-  String? hivMgmt2P;
-  String? hivMgmt2Q;
-  String? hivMgmt2R;
-  String? hivMgmt2S;
-  AppFormMetaData? appFormMetadata;
-  String? message;
+class UnApprovedHivManagementForm extends HivManagementFormModel {
+  final String? message;
+  final String? ovcCpimsId;
+  final String? caregiverCpimsId;
+  final String? adherenceId;
+  final AppFormMetaData appFormMetaData;
 
   UnApprovedHivManagementForm({
-    this.adherenceId,
-    this.ovcCpimsId,
-    this.hivMgmt1A,
-    this.hivMgmt1B,
-    this.hivMgmt1C,
-    this.hivMgmt1D,
-    this.hivMgmt1E,
-    this.hivMgmt1EDate,
-    this.hivMgmt1F,
-    this.hivMgmt1FDate,
-    this.hivMgmt1G,
-    this.hivMgmt1GDate,
-    this.hivMgmt2A,
-    this.hivMgmt2B,
-    this.hivMgmt2C,
-    this.hivMgmt2D,
-    this.hivMgmt2E,
-    this.hivMgmt2F,
-    this.hivMgmt2G,
-    this.hivMgmt2H2,
-    this.hivMgmt2H1,
-    this.hivMgmt2H3,
-    this.hivMgmt2H4,
-    this.hivMgmt2H5,
-    this.hivMgmt2I1,
-    this.hivMgmt2IDate,
-    this.hivMgmt2J,
-    this.hivMgmt2K,
-    this.hivMgmt2L1,
-    this.hivMgmt2L2,
-    this.hivMgmt2M,
-    this.hivMgmt2N,
-    this.hivMgmt2O1,
-    this.hivMgmt2O2,
-    this.hivMgmt2P,
-    this.hivMgmt2Q,
-    this.hivMgmt2R,
-    this.hivMgmt2S,
-    this.appFormMetadata,
     this.message,
-  });
+    this.ovcCpimsId,
+    this.caregiverCpimsId,
+    this.adherenceId,
+    required this.appFormMetaData,
+    String dateOfEvent = '',
+    String dateHIVConfirmedPositive = '',
+    String dateTreatmentInitiated = '',
+    String baselineHEILoad = '',
+    String dateStartedFirstLine = '',
+    String arvsSubWithFirstLine = '',
+    String arvsSubWithFirstLineDate = '',
+    String switchToSecondLine = '',
+    String switchToSecondLineDate = '',
+    String switchToThirdLine = '',
+    String switchToThirdLineDate = '',
+    String visitDate = '',
+    String durationOnARTs = '',
+    String height = '',
+    String mUAC = '',
+    String arvDrugsAdherence = '',
+    String arvDrugsDuration = '',
+    String adherenceCounseling = '',
+    String treatmentSupporter = '',
+    String treatmentSupporterSex = '',
+    String treatmentSupporterAge = '',
+    String treatmentSupporterHIVStatus = '',
+    String viralLoadResults = '',
+    String labInvestigationsDate = '',
+    String detectableViralLoadInterventions = '',
+    String disclosure = '',
+    String mUACScore = '',
+    String zScore = '',
+    List<String> nutritionalSupport = const [],
+    String supportGroupStatus = '',
+    String nhifEnrollment = '',
+    String nhifEnrollmentStatus = '',
+    String referralServices = '',
+    String nextAppointmentDate = '',
+    String peerEducatorName = '',
+    String peerEducatorContact = '',
+  }) : super(
+          dateOfEvent: dateOfEvent,
+          dateHIVConfirmedPositive: dateHIVConfirmedPositive,
+          dateTreatmentInitiated: dateTreatmentInitiated,
+          baselineHEILoad: baselineHEILoad,
+          dateStartedFirstLine: dateStartedFirstLine,
+          arvsSubWithFirstLine: arvsSubWithFirstLine,
+          arvsSubWithFirstLineDate: arvsSubWithFirstLineDate,
+          switchToSecondLine: switchToSecondLine,
+          switchToSecondLineDate: switchToSecondLineDate,
+          switchToThirdLine: switchToThirdLine,
+          switchToThirdLineDate: switchToThirdLineDate,
+          visitDate: visitDate,
+          durationOnARTs: durationOnARTs,
+          height: height,
+          mUAC: mUAC,
+          arvDrugsAdherence: arvDrugsAdherence,
+          arvDrugsDuration: arvDrugsDuration,
+          adherenceCounseling: adherenceCounseling,
+          treatmentSupporter: treatmentSupporter,
+          treatmentSupporterSex: treatmentSupporterSex,
+          treatmentSupporterAge: treatmentSupporterAge,
+          treatmentSupporterHIVStatus: treatmentSupporterHIVStatus,
+          viralLoadResults: viralLoadResults,
+          labInvestigationsDate: labInvestigationsDate,
+          detectableViralLoadInterventions: detectableViralLoadInterventions,
+          disclosure: disclosure,
+          mUACScore: mUACScore,
+          zScore: zScore,
+          nutritionalSupport: nutritionalSupport,
+          supportGroupStatus: supportGroupStatus,
+          nhifEnrollment: nhifEnrollment,
+          nhifEnrollmentStatus: nhifEnrollmentStatus,
+          referralServices: referralServices,
+          nextAppointmentDate: nextAppointmentDate,
+          peerEducatorName: peerEducatorName,
+          peerEducatorContact: peerEducatorContact,
+        );
 
   factory UnApprovedHivManagementForm.fromJson(Map<String, dynamic> json) {
+    final appFormMetaData = AppFormMetaData.fromJson(json['app_form_metadata']);
+
     return UnApprovedHivManagementForm(
-      adherenceId: json['adherence_id'] ?? '',
-      ovcCpimsId: json['ovc_cpims_id'] ?? 0,
-      hivMgmt1A: json['HIV_MGMT_1_A'] ?? '',
-      hivMgmt1B: json['HIV_MGMT_1_B'] ?? '',
-      hivMgmt1C: json['HIV_MGMT_1_C'] ?? '',
-      hivMgmt1D: json['HIV_MGMT_1_D'] ?? '',
-      hivMgmt1E: json['HIV_MGMT_1_E'],
-      hivMgmt1EDate: json['HIV_MGMT_1_E_DATE'] ?? '',
-      hivMgmt1F: json['HIV_MGMT_1_F'],
-      hivMgmt1FDate: json['HIV_MGMT_1_F_DATE'] ?? '',
-      hivMgmt1G: json['HIV_MGMT_1_G'] ?? '',
-      hivMgmt1GDate: json['HIV_MGMT_1_G_DATE'] ?? '',
-      hivMgmt2A: json['HIV_MGMT_2_A'],
-      hivMgmt2B: json['HIV_MGMT_2_B'],
-      hivMgmt2C: json['HIV_MGMT_2_C'],
-      hivMgmt2D: json['HIV_MGMT_2_D'] ?? '',
-      hivMgmt2E: json['HIV_MGMT_2_E'] ?? '',
-      hivMgmt2F: json['HIV_MGMT_2_F'] ?? '',
-      hivMgmt2G: json['HIV_MGMT_2_G'] ?? '',
-      hivMgmt2H2: json['HIV_MGMT_2_H_2'] ?? '',
-      hivMgmt2H1: json['HIV_MGMT_2_H_1'] ?? '',
-      hivMgmt2H3: json['HIV_MGMT_2_H_3'] ?? '',
-      hivMgmt2H4: json['HIV_MGMT_2_H_4'],
-      hivMgmt2H5: json['HIV_MGMT_2_H_5'] ?? '',
-      hivMgmt2I1: json['HIV_MGMT_2_I_1'],
-      hivMgmt2IDate: json['HIV_MGMT_2_I_DATE'] ?? '',
-      hivMgmt2J: json['HIV_MGMT_2_J'] ?? '',
-      hivMgmt2K: json['HIV_MGMT_2_K'] ?? '',
-      hivMgmt2L1: json['HIV_MGMT_2_L_1'] ?? '',
-      hivMgmt2L2: json['HIV_MGMT_2_L_2'] ?? '',
-      hivMgmt2M: List<String>.from(json['HIV_MGMT_2_M']
-          .replaceAll(r"\'", "")
-          .replaceAll(r"\[", "")
-          .replaceAll(r"\]", "")
-          .split(", ")),
-      hivMgmt2N: json['HIV_MGMT_2_N'] ?? '',
-      hivMgmt2O1: json['HIV_MGMT_2_O_1'],
-      hivMgmt2O2: json['HIV_MGMT_2_O_2'],
-      hivMgmt2P: json['HIV_MGMT_2_P'],
-      hivMgmt2Q: json['HIV_MGMT_2_Q'],
-      hivMgmt2R: json['HIV_MGMT_2_R'],
-      hivMgmt2S: json['HIV_MGMT_2_S'],
-      appFormMetadata: AppFormMetaData.fromJson(json['app_form_metadata']),
-      message: json['message'],
+      message: json['message'] ?? '',
+      ovcCpimsId: json['ovc_cpims_id'].toString(),
+      caregiverCpimsId: json['caregiver_cpims_id'] ?? '',
+      adherenceId: json['adherence_id'],
+      appFormMetaData: appFormMetaData,
+      dateHIVConfirmedPositive: json['HIV_MGMT_1_A'],
+      dateTreatmentInitiated: json['HIV_MGMT_1_B'],
+      baselineHEILoad: json['HIV_MGMT_1_C'],
+      dateStartedFirstLine: json['HIV_MGMT_1_D'],
+      arvsSubWithFirstLine: json['HIV_MGMT_1_E'].toString(),
+      arvsSubWithFirstLineDate: json['HIV_MGMT_1_E_DATE'],
+      switchToSecondLine: json['HIV_MGMT_1_F'].toString(),
+      switchToSecondLineDate: json['HIV_MGMT_1_F_DATE'],
+      switchToThirdLine: json['HIV_MGMT_1_G'].toString(),
+      switchToThirdLineDate: json['HIV_MGMT_1_G_DATE'],
+      visitDate: json['HIV_MGMT_2_A'],
+      durationOnARTs: json['HIV_MGMT_2_B'],
+      height: json['HIV_MGMT_2_C'],
+      mUAC: json['HIV_MGMT_2_D'] ?? "",
+      arvDrugsAdherence: json['HIV_MGMT_2_E'],
+      arvDrugsDuration: json['HIV_MGMT_2_F'],
+      adherenceCounseling: json['HIV_MGMT_2_G'],
+      treatmentSupporter: json['HIV_MGMT_2_H_2'],
+      treatmentSupporterSex: json['HIV_MGMT_2_H_3'],
+      treatmentSupporterAge: json['HIV_MGMT_2_H_4'],
+      treatmentSupporterHIVStatus: json['HIV_MGMT_2_H_5'],
+      viralLoadResults: json['HIV_MGMT_2_I_1'],
+      labInvestigationsDate: json['HIV_MGMT_2_I_DATE'],
+      detectableViralLoadInterventions: json['HIV_MGMT_2_J'],
+      disclosure: json['HIV_MGMT_2_K'],
+      mUACScore: json['HIV_MGMT_2_L_1'],
+      zScore: json['HIV_MGMT_2_L_2'],
+      nutritionalSupport: json['HIV_MGMT_2_M']
+          .toString()
+          .replaceAll('[', '')
+          .replaceAll(']', '')
+          .split(',')
+          .map((item) => item.trim().replaceAll("'", ""))
+          .toList(),
+      supportGroupStatus: json['HIV_MGMT_2_N'],
+      nhifEnrollment: json['HIV_MGMT_2_O_1'].toString(),
+      nhifEnrollmentStatus: json['HIV_MGMT_2_O_2'],
+      referralServices: json['HIV_MGMT_2_P'],
+      nextAppointmentDate: json['HIV_MGMT_2_Q'],
+      peerEducatorName: json['HIV_MGMT_2_R'],
+      peerEducatorContact: json['HIV_MGMT_2_S'],
     );
   }
 
-  //toJson
-  Map<String, dynamic> toJson() {
-    return {
-      'adherence_id': adherenceId,
-      'ovc_cpims_id': ovcCpimsId,
-      'HIV_MGMT_1_A': hivMgmt1A,
-      'HIV_MGMT_1_B': hivMgmt1B,
-      'HIV_MGMT_1_C': hivMgmt1C,
-      'HIV_MGMT_1_D': hivMgmt1D,
-      'HIV_MGMT_1_E': hivMgmt1E,
-      'HIV_MGMT_1_E_DATE': hivMgmt1EDate,
-      'HIV_MGMT_1_F': hivMgmt1F,
-      'HIV_MGMT_1_F_DATE': hivMgmt1FDate,
-      'HIV_MGMT_1_G': hivMgmt1G,
-      'HIV_MGMT_1_G_DATE': hivMgmt1GDate,
-      'HIV_MGMT_2_A': hivMgmt2A,
-      'HIV_MGMT_2_B': hivMgmt2B,
-      'HIV_MGMT_2_C': hivMgmt2C,
-      'HIV_MGMT_2_D': hivMgmt2D,
-      'HIV_MGMT_2_E': hivMgmt2E,
-      'HIV_MGMT_2_F': hivMgmt2F,
-      'HIV_MGMT_2_G': hivMgmt2G,
-      'HIV_MGMT_2_H_2': hivMgmt2H2,
-      'HIV_MGMT_2_H_1': hivMgmt2H1,
-      'HIV_MGMT_2_H_3': hivMgmt2H3,
-      'HIV_MGMT_2_H_4': hivMgmt2H4,
-      'HIV_MGMT_2_H_5': hivMgmt2H5,
-      'HIV_MGMT_2_I_1': hivMgmt2I1,
-      'HIV_MGMT_2_I_DATE': hivMgmt2IDate,
-      'HIV_MGMT_2_J': hivMgmt2J,
-      'HIV_MGMT_2_K': hivMgmt2K,
-      'HIV_MGMT_2_L_1': hivMgmt2L1,
-      'HIV_MGMT_2_L_2': hivMgmt2L2,
-      'HIV_MGMT_2_M': '["${hivMgmt2M?.join('", "')}"]',
-      'HIV_MGMT_2_N': hivMgmt2N,
-      'HIV_MGMT_2_O_1': hivMgmt2O1,
-      'HIV_MGMT_2_O_2': hivMgmt2O2,
-      'HIV_MGMT_2_P': hivMgmt2P,
-      'HIV_MGMT_2_Q': hivMgmt2Q,
-      'HIV_MGMT_2_R': hivMgmt2R,
-      'HIV_MGMT_2_S': hivMgmt2S,
-      'app_form_metadata': appFormMetadata?.toJson(),
-      'message': message,
-    };
-  }
-
   @override
-  String toString() {
-    return 'HivManagementForm('
-        'adherenceId: $adherenceId, '
-        'ovcCpimsId: $ovcCpimsId, '
-        'hivMgmt1A: $hivMgmt1A, '
-        'hivMgmt1B: $hivMgmt1B, '
-        'hivMgmt1C: $hivMgmt1C, '
-        'hivMgmt1D: $hivMgmt1D, '
-        'hivMgmt1E: $hivMgmt1E, '
-        'hivMgmt1EDate: $hivMgmt1EDate, '
-        'hivMgmt1F: $hivMgmt1F, '
-        'hivMgmt1FDate: $hivMgmt1FDate, '
-        'hivMgmt1G: $hivMgmt1G, '
-        'hivMgmt1GDate: $hivMgmt1GDate, '
-        'hivMgmt2A: $hivMgmt2A, '
-        'hivMgmt2B: $hivMgmt2B, '
-        'hivMgmt2C: $hivMgmt2C, '
-        'hivMgmt2D: $hivMgmt2D, '
-        'hivMgmt2E: $hivMgmt2E, '
-        'hivMgmt2F: $hivMgmt2F, '
-        'hivMgmt2G: $hivMgmt2G, '
-        'hivMgmt2H2: $hivMgmt2H2, '
-        'hivMgmt2H1: $hivMgmt2H1, '
-        'hivMgmt2H3: $hivMgmt2H3, '
-        'hivMgmt2H4: $hivMgmt2H4, '
-        'hivMgmt2H5: $hivMgmt2H5, '
-        'hivMgmt2I1: $hivMgmt2I1, '
-        'hivMgmt2IDate: $hivMgmt2IDate, '
-        'hivMgmt2J: $hivMgmt2J, '
-        'hivMgmt2K: $hivMgmt2K, '
-        'hivMgmt2L1: $hivMgmt2L1, '
-        'hivMgmt2L2: $hivMgmt2L2, '
-        'hivMgmt2M: $hivMgmt2M, '
-        'hivMgmt2N: $hivMgmt2N, '
-        'hivMgmt2O1: $hivMgmt2O1, '
-        'hivMgmt2O2: $hivMgmt2O2, '
-        'hivMgmt2P: $hivMgmt2P, '
-        'hivMgmt2Q: $hivMgmt2Q, '
-        'hivMgmt2R: $hivMgmt2R, '
-        'hivMgmt2S: $hivMgmt2S, '
-        'appFormMetadata: $appFormMetadata, '
-        'message: $message)';
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = super.toJson();
+    data['message'] = message;
+    data['ovc_cpims_id'] = ovcCpimsId;
+    data['caregiver_cpims_id'] = caregiverCpimsId;
+    data['adherence_id'] = adherenceId;
+    data['app_form_metadata'] = appFormMetaData.toJson();
+    return data;
   }
 }
--- lib/services/unapproved_data_service.dart
+++ lib/services/unapproved_data_service.dart
@@ -90,60 +90,16 @@ class UnapprovedDataService {
       } else if (endpoint == endpoints[4]) {
         for (var map in jsonData) {
           final unapprovedHmf = UnApprovedHivManagementForm.fromJson(map);
-          // final artTherapyHIVFormModel = ARTTherapyHIVFormModel(
-          //   dateHIVConfirmedPositive: unapprovedHmf.hivMgmt1A ?? "",
-          //   dateTreatmentInitiated: unapprovedHmf.hivMgmt1B ?? "",
-          //   baselineHEILoad: unapprovedHmf.hivMgmt1C ?? "",
-          //   dateStartedFirstLine: unapprovedHmf.hivMgmt1D ?? "",
-          //   arvsSubWithFirstLine: unapprovedHmf.hivMgmt1E.toString() ?? "",
-          //   arvsSubWithFirstLineDate: unapprovedHmf.hivMgmt1EDate ?? "",
-          //   switchToSecondLine: unapprovedHmf.hivMgmt1F.toString() ?? "",
-          //   switchToSecondLineDate: unapprovedHmf.hivMgmt1FDate ?? "",
-          //   switchToThirdLine: unapprovedHmf.hivMgmt1G.toString() ?? "",
-          //   switchToThirdLineDate: unapprovedHmf.hivMgmt1GDate ?? "",
-          // );
-          //
-          // final hivVisitationFormModel = HIVVisitationFormModel(
-          //   visitDate: unapprovedHmf.hivMgmt2A ?? "",
-          //   durationOnARTs: unapprovedHmf.hivMgmt2B.toString() ?? "",
-          //   height: unapprovedHmf.hivMgmt2C.toString() ?? "",
-          //   mUAC: unapprovedHmf.hivMgmt2D ?? "",
-          //   arvDrugsAdherence: unapprovedHmf.hivMgmt2E ?? "",
-          //   arvDrugsDuration: unapprovedHmf.hivMgmt2F ?? "",
-          //   adherenceCounseling: unapprovedHmf.hivMgmt2G ?? "",
-          //   treatmentSupporter: unapprovedHmf.hivMgmt2H2 ?? "",
-          //   treatmentSupporterSex: unapprovedHmf.hivMgmt2H3 ?? "",
-          //   treatmentSupporterAge: unapprovedHmf.hivMgmt2H4.toString() ?? "",
-          //   treatmentSupporterHIVStatus: unapprovedHmf.hivMgmt2H5 ?? "",
-          //   viralLoadResults: unapprovedHmf.hivMgmt2I1.toString() ?? "",
-          //   labInvestigationsDate: unapprovedHmf.hivMgmt2IDate ?? "",
-          //   detectableViralLoadInterventions: unapprovedHmf.hivMgmt2J ?? "",
-          //   disclosure: unapprovedHmf.hivMgmt2K ?? "",
-          //   mUACScore: unapprovedHmf.hivMgmt2L1 ?? "",
-          //   zScore: unapprovedHmf.hivMgmt2L2 ?? "",
-          //   nutritionalSupport:
-          //       unapprovedHmf.hivMgmt2M?.whereType<String>().toList() ?? [],
-          //   supportGroupStatus: unapprovedHmf.hivMgmt2N ?? "",
-          //   nhifEnrollment: unapprovedHmf.hivMgmt2O1.toString() ?? "",
-          //   nhifEnrollmentStatus: unapprovedHmf.hivMgmt2O2 ?? "",
-          //   referralServices: unapprovedHmf.hivMgmt2P ?? "",
-          //   nextAppointmentDate: unapprovedHmf.hivMgmt2Q ?? "",
-          //   peerEducatorName: unapprovedHmf.hivMgmt2R ?? "",
-          //   peerEducatorContact: unapprovedHmf.hivMgmt2S ?? "",
-          // );
-
-          // db.insertHMFFormData(
-          //   unapprovedHmf.ovcCpimsId.toString(),
-          //   null,
-          //   artTherapyHIVFormModel,
-          //   hivVisitationFormModel,
-          //   unapprovedHmf.appFormMetadata?.formId,
-          //   unapprovedHmf.appFormMetadata?.startOfInterview,
-          //   unapprovedHmf.appFormMetadata?.formType,
-          //   true,
-          //   //
-          //   null, // rejectedMessage is null
-          // );
+          db.insertHMFFormData(
+            unapprovedHmf.ovcCpimsId.toString(),
+            null,
+            unapprovedHmf,
+            unapprovedHmf.appFormMetaData.formId,
+            unapprovedHmf.appFormMetaData.startOfInterview,
+            unapprovedHmf.appFormMetaData.formType,
+            true,
+            unapprovedHmf.message, // rejectedMessage is null
+          );
         }
       } else if (endpoint == endpoints[5]) {
         //fetch unapproved hrs
@@ -211,7 +167,8 @@ class UnapprovedDataService {
     return unapprovedCptList;
   }
 
-  static Future<List<UnApprovedHivManagementForm>> fetchRejectedHMFForms() async {
+  static Future<List<UnApprovedHivManagementForm>>
+      fetchRejectedHMFForms() async {
     final db = LocalDb.instance;
     List<Map<String, dynamic>> maps = await db.fetchRejectedHMFFormData();
     List<UnApprovedHivManagementForm> unapprovedHmfData = [];
