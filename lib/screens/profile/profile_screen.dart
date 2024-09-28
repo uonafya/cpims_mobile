cb9c7e4b46b1cdef751ebbc9ec5c8f99b59d603b
RyanKoech <sirryankoech@gmail.com>
2024-09-28 12:03:49 +0300

    add profile stats

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -24,108 +24,182 @@ class _ProfileScreenState extends State<ProfileScreen> {
       drawer: const Drawer(
         child: CustomDrawer(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Image.network(
-              "",
-              width: double.infinity,
-              fit: BoxFit.fitWidth,
-              errorBuilder:
-                  (BuildContext context, Object exception, StackTrace? stackTrace) {
-                return const Image(
-                    image: AssetImage('assets/images/user-2.jpg'));
-              },
-            ),
-            const SizedBox(height: 10,),
-            CustomCard(title: "ACCOUNT DETAILS", children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                      "Username:",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Flexible(child: Text(dashData.username)),
-                ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Image.network(
+                "",
+                width: double.infinity,
+                fit: BoxFit.fitWidth,
+                errorBuilder:
+                    (BuildContext context, Object exception, StackTrace? stackTrace) {
+                  return const Image(
+                      image: AssetImage('assets/images/user-2.jpg'));
+                },
               ),
-              const SizedBox(height: 4,),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "CPIMS ID:",
-                    style: TextStyle(
+              const SizedBox(height: 10,),
+              CustomCard(title: "ACCOUNT DETAILS", children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                        "Username:",
+                      style: TextStyle(
                         fontWeight: FontWeight.bold
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Flexible(child: Text(dashData.userCpimsId.toString()))
-                ],
-              ),
-              const SizedBox(height: 4,),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Email Address:",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
+                    const SizedBox(width: 10,),
+                    Flexible(child: Text(dashData.username)),
+                  ],
+                ),
+                const SizedBox(height: 4,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "CPIMS ID:",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Flexible(child: Text(dashData.email)),
-                ],
-              ),
-              const SizedBox(height: 4,),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Sub-county:",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
+                    const SizedBox(width: 10,),
+                    Flexible(child: Text(dashData.userCpimsId.toString()))
+                  ],
+                ),
+                const SizedBox(height: 4,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Email Address:",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Flexible(child: Text(dashData.subCounties)),
-                ],
-              ),
-              const SizedBox(height: 4,),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Ward:",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
+                    const SizedBox(width: 10,),
+                    Flexible(child: Text(dashData.email)),
+                  ],
+                ),
+                const SizedBox(height: 4,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Sub-county:",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Flexible(child: Text(dashData.ward)),
-                ],
-              ),
-              const SizedBox(height: 4,),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Organization Unit(s):",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
+                    const SizedBox(width: 10,),
+                    Flexible(child: Text(dashData.subCounties)),
+                  ],
+                ),
+                const SizedBox(height: 4,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Ward:",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
                     ),
+                    const SizedBox(width: 10,),
+                    Flexible(child: Text(dashData.ward)),
+                  ],
+                ),
+                const SizedBox(height: 4,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Organization Unit(s):",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    const SizedBox(width: 10,),
+                    Flexible(child: Text(dashData.userOrgUnits)),
+                    // Text("MAKADARA SUB COUNTY CHILDREN OFFICE")
+                  ],
+                ),
+              ]),
+              const SizedBox(height: 10,),
+              CustomCard(title: "CASELOAD STATISTICS", children: [
+                Container(
+                  color: Colors.white70,
+                  child: Table(
+                    border: TableBorder.all(), // Adds borders around the table cells
+                    children: [
+                      TableRow(
+                          children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'OVC',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(dashData.caseloadOvc > 0 ? dashData.caseloadOvc.toString() : "N/A"),
+                        ),
+                      ]),
+                      TableRow(children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'HH',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(dashData.caseloadHh > 0 ? dashData.caseloadHh.toString() : "N/A"),
+                        ),
+                      ]),
+                      TableRow(children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'CLHIV',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(dashData.caseloadClhiv > 0 ? dashData.caseloadClhiv.toString() : "N/A"),
+                        ),
+                      ]),
+                      TableRow(children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'HEI',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(dashData.caseloadHei > 0 ? dashData.caseloadHei.toString() : "N/A"),
+                        ),
+                      ]),
+                    ],
                   ),
-                  const SizedBox(width: 10,),
-                  Flexible(child: Text(dashData.userOrgUnits)),
-                  // Text("MAKADARA SUB COUNTY CHILDREN OFFICE")
-                ],
-              ),
-            ]),
-          ],
+                )
+              ]),
+            ],
+          ),
         ),
       ),
     );
