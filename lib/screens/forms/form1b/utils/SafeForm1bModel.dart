24ce64856ef2600952764299f70b97449a16b685
MainaKarl <mainakarlg@gmail.com>
2024-10-15 16:24:52 +0300

    Form1b safe form model updated

--- lib/screens/forms/form1b/utils/SafeForm1bModel.dart
+++ lib/screens/forms/form1b/utils/SafeForm1bModel.dart
@@ -5,4 +5,23 @@ class SafeFormData {
   late String domainId;
 
   SafeFormData({required this.selectedServices, required this.domainId});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'selectedServices': selectedServices.map((item) => item.toJson()).toList(),
+      'domainId': domainId,
+    };
+  }
+
+  factory SafeFormData.fromJson(Map<String, dynamic> json) {
+    return SafeFormData(
+      selectedServices: json['selectedServices'].map((item) => ValueItem.fromJson(item)).toList(),
+      domainId: json['domainId'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'SafeFormData{selectedServices: $selectedServices, domainId: $domainId}';
+  }
 }
