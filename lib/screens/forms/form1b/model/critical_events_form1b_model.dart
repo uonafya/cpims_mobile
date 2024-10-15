0845c75413cdc50e6d1b843cceebdd51f42eba4d
MainaKarl <mainakarlg@gmail.com>
2024-10-15 16:24:28 +0300

    Form1b critical event model updated

--- lib/screens/forms/form1b/model/critical_events_form1b_model.dart
+++ lib/screens/forms/form1b/model/critical_events_form1b_model.dart
@@ -5,4 +5,23 @@ class CriticalEventDataForm1b {
   String selectedDate;
 
   CriticalEventDataForm1b({required this.selectedEvents, required this.selectedDate});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'selectedEvents': selectedEvents.map((item) => item.toJson()).toList(),
+      'selectedDate': selectedDate,
+    };
+  }
+
+  factory CriticalEventDataForm1b.fromJson(Map<String, dynamic> json) {
+    return CriticalEventDataForm1b(
+      selectedEvents: json['selectedEvents'].map((item) => ValueItem.fromJson(item)).toList(),
+      selectedDate: json['selectedDate'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'CriticalEventDataForm1b{selectedEvents: $selectedEvents, selectedDate: $selectedDate}';
+  }
 }
